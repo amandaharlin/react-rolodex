4135cbdae3d99183f204cde056e5e9fb7478bed6
Amanda Harlin <amanda.n.harlin@gmail.com>
2018-04-16 22:56:56 -0500

    adding the contact input

---
+++ mockContacts.js
@@ -0,0 +1,4 @@
+export const mockContacts = [
+  { id: 1, name: 'Amanda', email: 'test@test.io' },
+  { id: 2, name: 'Ada', email: 'ada@test.io' }
+];
--- src/App.js
+++ src/App.js
@@ -1,18 +1,79 @@
+import 'semantic-ui-css/semantic.min.css';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Container,
+  Divider,
+  Form,
+  Header,
+  Icon,
+  List
+} from 'semantic-ui-react';
+
+//import './allContacts.js';
+
 
 class App extends Component {
+  state = { name: '', email: '', allContacts: '' };
+
+  renderContactInputForm = () => {
+    return (
+      <div>
+        <Form>
+          <Form.Group>
+            <Form.Input
+              placeholder="name"
+              name="name"
+              value={name}
+              onChange={(event, data) => {
+                this.setState({ name: data.value });
+                console.log(name);
+              }}
+            />
+            <Form.Input
+              placeholder="email"
+              name="email"
+              value={email}
+              onChange={(event, data) => {
+                this.setState({ email: data.value });
+              }}
+            />
+          </Form.Group>
+        </Form>
+        <Button
+          color="green"
+          onClick={() => {
+            const newContact = {
+              id: Math.random(),
+              name: name,
+              email: email
+            };
+
+            this.setState({
+              // allContacts: [newContact, ...this.state.allContacts]
+            });
+          }}
+        >
+          <Icon name="checkmark" /> Add This Contact
+        </Button>
+      </div>
+    );
+  };
+
+  renderContactList = () => {};
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Header>Rolodex</Header>
+          {/* {this.renderContactInputForm()} */}
+          <Divider />
+          <Header>Contacts</Header>
+          {/* {this.renderContactList()} */}
+        </Container>
       </div>
     );
   }
